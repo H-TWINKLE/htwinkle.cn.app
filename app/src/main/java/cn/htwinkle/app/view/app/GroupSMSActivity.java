de477f0982e41adfa5e7aad4244094f67e435e18
TWINKLE <zhanghjqqqq@163.com>
2023-01-21 22:01:03 +0800

    支持群发短信排序功能

--- app/src/main/java/cn/htwinkle/app/view/app/GroupSMSActivity.java
+++ app/src/main/java/cn/htwinkle/app/view/app/GroupSMSActivity.java
@@ -10,8 +10,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.yanzhenjie.permission.runtime.Permission;
+
+import net.sourceforge.pinyin4j.PinyinHelper;
+import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
+import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
+import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
 
 import org.xutils.ex.DbException;
 import org.xutils.view.annotation.ContentView;
@@ -128,7 +136,13 @@ public class GroupSMSActivity extends BaseRefreshActivity<SmsPerson, SmsPersonAd
      * 组合数据和缓存数据
      */
     private void combineData() {
-        List<SmsPerson> smsPeople = PhoneKit.INSTANCE.getPhone(this);
+        List<SmsPerson> smsPeople = PhoneKit.INSTANCE.getPhone(this).stream()
+                .sorted((t0, t1) -> {
+                    if (TextUtils.isEmpty(t0.getName()) || TextUtils.isEmpty(t1.getName())) {
+                        return -1;
+                    }
+                    return getPinYinName(t0.getName()).compareTo(getPinYinName(t1.getName()));
+                }).collect(Collectors.toList());
         smsPeople = smsPeople.stream().filter(item ->
                 !TextUtils.isEmpty(item.getName()) && item.getName().length() < 4
         ).collect(Collectors.toList());
@@ -185,14 +199,61 @@ public class GroupSMSActivity extends BaseRefreshActivity<SmsPerson, SmsPersonAd
                 for (int i = 0; i < strings.size(); i++) {
                     manager.sendTextMessage(tel, null, strings.get(i), null, null);
                 }
-                setToastString(index + " . " + name + " 发送短信成功 " + tel);
+                setToastString(getSenderToast(index, allCount, name) + "成功 " + tel);
                 if (index == allCount) {
                     setToastString(String.format("发送完成，合计发送 %s 条", allCount));
                     base_rich_fab.setEnabled(true);
                 }
             }, 2000);
         } catch (Exception e) {
-            setToastString(index + " . " + name + " 发送短信失败：" + e.getLocalizedMessage());
+            setToastString(getSenderToast(index, allCount, name) + "失败：" + e.getLocalizedMessage());
         }
+    }
+
+    /**
+     * 发送信息提示
+     *
+     * @param index    index
+     * @param allCount allCount
+     * @param name     name
+     * @return String
+     */
+    @NonNull
+    private String getSenderToast(int index, int allCount, String name) {
+        return index + "/" + allCount + " . " + name + " 发送短信";
+    }
+
+    private String getPinYinName(String text) {
+        if (TextUtils.isEmpty(text)) {
+            return "";
+        }
+        return getFirstLetter(text);
+    }
+
+    /**
+     * 取第一个汉字的第一个字符
+     *
+     * @return String
+     */
+    public static String getFirstLetter(String ChineseLanguage) {
+        char[] cl_chars = ChineseLanguage.trim().toCharArray();
+        String hanyupinyin = "";
+        HanyuPinyinOutputFormat defaultFormat = new HanyuPinyinOutputFormat();
+        defaultFormat.setCaseType(HanyuPinyinCaseType.UPPERCASE);// 输出拼音全部大写
+        defaultFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);// 不带声调
+        try {
+            String str = String.valueOf(cl_chars[0]);
+            if (str.matches("[\u4e00-\u9fa5]+")) {// 如果字符是中文,则将中文转为汉语拼音,并取第一个字母
+                hanyupinyin = PinyinHelper.toHanyuPinyinStringArray(
+                        cl_chars[0], defaultFormat)[0].substring(0, 1);
+            } else if (str.matches("[0-9]+")) {// 如果字符是数字,取数字
+                hanyupinyin += cl_chars[0];
+            } else if (str.matches("[a-zA-Z]+")) {// 如果字符是字母,取字母
+
+                hanyupinyin += cl_chars[0];
+            }
+        } catch (BadHanyuPinyinOutputFormatCombination ignored) {
+        }
+        return hanyupinyin;
     }
 }
