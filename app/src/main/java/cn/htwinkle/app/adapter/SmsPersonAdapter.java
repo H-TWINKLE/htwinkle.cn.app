c3b60d5f850f0d7eca65993d5a6864014baf7e50
TWINKLE <zhanghjqqqq@163.com>
2023-01-29 15:29:29 +0800

    支持备份处理

--- app/src/main/java/cn/htwinkle/app/adapter/SmsPersonAdapter.java
+++ app/src/main/java/cn/htwinkle/app/adapter/SmsPersonAdapter.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.text.InputType;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -115,7 +116,8 @@ public class SmsPersonAdapter extends BaseQuickAdapter<SmsPerson, BaseViewHolder
             CommKit.safety(() -> CommKit.POOL_EXECUTOR.submit(() -> {
                 SmsGroupOut smsGroupOut = new SmsGroupOut(smsPerson, activity);
                 if (StrUtil.isNotEmpty(smsGroupOut.getTelSn())) {
-                    HttpUtil.post(HttpConstant.SMS_SAVE_OR_UPDATE, JSONObject.toJSONString(smsGroupOut));
+                    String saveBackUp = HttpUtil.post(HttpConstant.SMS_SAVE_OR_UPDATE, JSONObject.toJSONString(smsGroupOut));
+                    // Log.i(TAG, "saveInfo: " + saveBackUp);
                 }
             }));
         }
@@ -124,7 +126,8 @@ public class SmsPersonAdapter extends BaseQuickAdapter<SmsPerson, BaseViewHolder
             CommKit.safety(() -> CommKit.POOL_EXECUTOR.submit(() -> {
                 SmsGroupOut smsGroupOut = new SmsGroupOut(smsPerson, activity);
                 if (StrUtil.isNotEmpty(smsGroupOut.getTelSn())) {
-                    HttpUtil.post(HttpConstant.SMS_DELETE_BY, JSONObject.toJSONString(smsGroupOut));
+                    String deleteBackup = HttpUtil.post(HttpConstant.SMS_DELETE_BY, JSONObject.toJSONString(smsGroupOut));
+                    // Log.i(TAG, "saveInfo: " + deleteBackup);
                 }
             }));
         }
