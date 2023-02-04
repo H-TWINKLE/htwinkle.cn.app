4a395597b7fc4969bc8253b76bc7d72e5ce4974e
TWINKLE <zhanghjqqqq@163.com>
2023-02-04 23:06:36 +0800

    新增工作流的打包处理

--- build.ps1
+++ build.ps1
@@ -1,6 +1,5 @@
 $versionPath=$PSScriptRoot+"/version.properties"
-$versionXml=([xml](Get-Content $versionPath))
-$versionProperty=$versionXml
+$versionProperty = convertfrom-stringdata (get-content $versionPath -raw)
 $ReleaseVersion=$versionProperty.VERSION_NAME+"."+$versionProperty.VERSION_CODE
 $ReleaseVersion
 Add-Content -Path $env:GITHUB_ENV -Value "ReleaseVersion=${ReleaseVersion}"
